9640e6e08fabeebfd833bc1099ef6a1ff07755be
renzop5231 <renzop5231@gmail.com>
2017-05-08 15:10:02 -0700

    dependency update

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require("webpack");
+const path = require("path");
+
+var jF = path.resolve(__dirname, "js");
+var bF = path.resolve(__dirname, "build");
+
+var config = {
+    entry: {
+        "order":jF+"/order.js",
+        "login":jF+"/login.js"
+    },
+    output:{
+        filename:"[name]bundle.js",
+        path:bF
+    },
+    plugins:[
+        new webpack.ProvidePlugin({
+            $:"jquery",
+            jQuery:"jquery"
+        })
+    ]
+};
+
+module.exports = config;
