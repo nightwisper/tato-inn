143dda3fdb49c3475438c644de5977aea7e6df35
KabilanMT <kabilanm.thangarajah@gmail.com>
2017-05-11 15:10:16 -0700

    Added Kitchen.html with css and JS in one file. Not working when I split up the js and the html. assuming it has something to do with the onclick. Been working on it for a while now. It'd be cool if someone could figureit out lol. Need to start working on merging it with the database
    Signed-off-by: KabilanMT <kabilanm.thangarajah@gmail.com>

--- build/kitchenbundle.js
+++ build/kitchenbundle.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -10327,48 +10327,82 @@ return jQuery;
 
 
 /***/ }),
-/* 1 */,
-/* 2 */
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-/* WEBPACK VAR INJECTION */(function($) {/**
- * Created by renzo on 2017-05-09.
- */
-$(document).ready(function(){
+/* WEBPACK VAR INJECTION */(function($, jQuery) {document.getElementsByClassName("cookButDiv").addEventListener("click", cooktime(this), false);
 
-    // document.getElementById("loginBut").addEventListener("click", function(){
-    //     $.ajax({
-    //         url:"/register",
-    //         type:"post",
-    //         data:{
-    //             user:"Admin",
-    //             un:document.getElementById("Username").value,
-    //             pass:document.getElementById("Password").value
-    //         },
-    //         success:function(resp){
-    //             console.log(resp);
-    //         }
-    //     })
-    // });
 
-    document.getElementById("loginBut").addEventListener("click", function(){
-        $.ajax({
-            url:"/login",
-            type: "post",
-            data:{
-                un:document.getElementById("Username").value,
-                pass:document.getElementById("Password").value
-            },
-            success:function(resp){
-                console.log(resp);
-                location.href = "/order";
-            }
+var finishDiv = document.getElementById("fulfilled");
 
-        });
-    });
+                function cooktime(elem){
+                    elem.onclick = "NOPE";
+                    elem.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/5/54/Blank_Canvas_on_Transparent_Background.png')";
+
+                    var progContainer = document.createElement("div");
+                    var progBar = document.createElement("div");
+                    var progBarComplete = document.createElement("div");
+                    progContainer.className = "col-xs-12 col-sm-12 progress-container";
+                    progBar.className = "progress progress-striped active";
+                    progBarComplete.className = "progress-bar progress-bar-info";
+                    progBarComplete.style.width = "0%";
+                    elem.append(progContainer);
+                    progContainer.appendChild(progBar);
+                    progBar.appendChild(progBarComplete);
+
+
+                    $(".progress-bar").animate({
+                        width: "100%"
+                    }, 5000);  
+
+                    var counter = 0;
+                    var interval = setInterval(function() {
+                        counter++;
+                        if (counter == 5) { //change the 5 here to the calculated time 
+                            progBarComplete.className += "progress-bar-complete";
+                        }
+                        if(counter == 6){ //change the 6 here to the calculated time + 1
+                            clearInterval(interval);
+                            var mainDiv = "#orderdiv" + elem.id;
+                            console.log(mainDiv); 
+            
+                            console.log($(mainDiv +' div:eq(2)').html()); 
+                            var ndiv = document.createElement("div");
+                            var countndiv = document.createElement("div");
+                            ndiv.innerHTML = " <hr /> <br /> <b>Order: " + elem.id + "</b><br /> Items: <br />" + $(mainDiv +' div:eq(2)').html();
+                            ndiv.id = "#finisheddiv" + elem.id;
+                            ndiv.append(countndiv);
+                            finishDiv.append(ndiv);            
+                            elem.parentNode.remove();
+                            function startTimer(duration, display) {
+                                var timer = duration, minutes, seconds;
+                                setInterval(function () {
+                                    minutes = parseInt(timer / 60, 10)
+                                    seconds = parseInt(timer % 60, 10);
+
+                                    minutes = minutes < 10 ? "0" + minutes : minutes;
+                                    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                                    display.text(minutes + ":" + seconds);
+
+                                    if (--timer < 0) {
+                                        timer = duration;
+                                    }
+                                }, 1000);
+                            }
+
+                            jQuery(function ($) {
+                                var fiveMinutes = 60 * 5,
+                                    display = $(countndiv);
+                                startTimer(fiveMinutes, display);
+                            });
+                        }
+                    }, 1150);                
+                }
+$(document).ready(function() {
+    
 });
-
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0), __webpack_require__(0)))
 
 /***/ })
 /******/ ]);
--- js/login.js
+++ js/login.js
@@ -1,23 +1,23 @@
-// /**
-//  * Created by renzo on 2017-05-09.
-//  */
+/**
+ * Created by renzo on 2017-05-09.
+ */
 $(document).ready(function(){
-//
-//     // document.getElementById("loginBut").addEventListener("click", function(){
-//     //     $.ajax({
-//     //         url:"/register",
-//     //         type:"post",
-//     //         data:{
-//     //             user:"Admin",
-//     //             un:document.getElementById("Username").value,
-//     //             pass:document.getElementById("Password").value
-//     //         },
-//     //         success:function(resp){
-//     //             console.log(resp);
-//     //         }
-//     //     })
-//     // });
-//
+
+    // document.getElementById("loginBut").addEventListener("click", function(){
+    //     $.ajax({
+    //         url:"/register",
+    //         type:"post",
+    //         data:{
+    //             user:"Admin",
+    //             un:document.getElementById("Username").value,
+    //             pass:document.getElementById("Password").value
+    //         },
+    //         success:function(resp){
+    //             console.log(resp);
+    //         }
+    //     })
+    // });
+
     document.getElementById("loginBut").addEventListener("click", function(){
         $.ajax({
             url:"/login",
