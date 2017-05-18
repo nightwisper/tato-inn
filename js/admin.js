377a47fce02d93d77831fe322ac8cd24abba15cc
KennyMango <kennethsitu@gmail.com>
2017-05-18 09:39:02 -0700

    Feature/fix (#75)
    * Implemented OrderCart & refractored existing order page
    
    Signed-off-by: kennymango <kennethsitu@gmail.com>
    
    * resolve merge conflict
    
    Signed-off-by: kennymango <kennethsitu@gmail.com>
    
    * fix
    
    Signed-off-by: kennymango <kennethsitu@gmail.com>
    
    * fix
    
    Signed-off-by: kennymango <kennethsitu@gmail.com>
    
    * fix
    
    Signed-off-by: kennymango <kennethsitu@gmail.com>

--- js/admin.js
+++ js/admin.js
@@ -25,68 +25,6 @@
 
     };
 
-    $(document).ready(function () {
-        mainApp.main_fun();
-
-        document.getElementById("reg_but").addEventListener("click", function () {
-            $.ajax({
-                url: "/register",
-                type: "post",
-                data: {
-                    username: document.getElementById("created_username").value,
-                    user: document.getElementById("user_option").value,
-                    pass: document.getElementById("created_password").value,
-                    con_pass: document.getElementById("con_created_password").value
-                },
-                success: function (resp) {
-                    console.log(resp);
-                }
-            });
-        });
-    });
-
 }(jQuery));
 
 
- // var register = document.getElementById("reg_but");
- // $(document).ready(function() {
- //
- //     if (register) {
- //         console.log("checkpoint1");
- //         register.addEventListener("click", function () {
- //             $.ajax({
- //                 url: "/register",
- //                 type: "post",
- //                 data: {
- //                     un: document.getElementById("created_username").value,
- //                     user: document.getElementById("user_option").value,
- //                     pass: document.getElementById("created_password").value,
- //                     con_pass: document.getElementById("con_created_password").value
- //                 },
- //                 success: function (resp) {
- //                     console.log(resp);
- //                 }
- //             });
- //         });
- //
- //     }
- //     else {
- //         console.log("fail");
- //         console.log("checkpoint1");
- //         register.addEventListener("click", function () {
- //             $.ajax({
- //                 url: "/register",
- //                 type: "post",
- //                 data: {
- //                     un: document.getElementById("created_username").value,
- //                     user: document.getElementById("user_option").value,
- //                     pass: document.getElementById("created_password").value,
- //                     con_pass: document.getElementById("con_created_password").value
- //                 },
- //                 success: function (resp) {
- //                     console.log(resp);
- //                 }
- //             });
- //         });
- //     }
- // });
